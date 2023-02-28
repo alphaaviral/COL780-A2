d62fc9befde63acb605504a01ed3ee05a48d69f5
alphaaviral <alphaaviral@gmail.com>
2023-03-01 01:49:00 +0530

    used scipy, numpy, removed svd

--- hessian.py
+++ hessian.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 import multiprocessing
-import scipy.signal as sc
 import time
 import csv
 from scipy.ndimage import maximum_filter as maxf2D
+from scipy.ndimage import convolve as cnv
 
 def calculateIntegral(image):
     width = image.shape[1]
@@ -48,9 +48,9 @@ def calculateDerivatives(image, xaxis, yaxis):
     xDer = np.array([[-1, 0, 1]], np.float64)
     yDer = np.array([[1], [0], [-1]], np.float64)
     if xaxis:
-        Ix = sc.convolve2d(image, xDer,mode='same')
+        Ix = cnv(image, xDer, mode='constant')
     if yaxis:
-        Iy = sc.convolve2d(image, yDer,mode='same')
+        Iy = cnv(image, yDer, mode='constant')
     if xaxis and yaxis:
         return Ix,Iy
     elif xaxis:
@@ -65,48 +65,65 @@ def calculateHessian(image, patch):
     Ixx, Ixy = calculateDerivatives(Ix, True, True)
     Iyy = calculateDerivatives(Iy, False, True)
     print(time.time() - start_time)
-    print("Calculating integral...")
-    start_time = time.time()
-    Ixxint = calculateIntegral(Ixx)
-    Ixyint = calculateIntegral(Ixy)
-    Iyyint = calculateIntegral(Iyy)
-    print(time.time() - start_time)
-    hessian = np.zeros((image.shape[0], image.shape[1], 2, 2))
-    print("Calculating hessian...")
-    start_time = time.time()
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            xmax = i+int(patch/2)
-            ymax = j+int(patch/2)
-            xmin = i-int(patch/2)-1
-            ymin = j-int(patch/2)-1
-            if xmax>=image.shape[0]:
-                xmax = image.shape[0]-1
-            if ymax>=image.shape[1]:
-                ymax = image.shape[1]-1
+    # print("Calculating integral...")
+    # start_time = time.time()
+    # Ixxint = calculateIntegral(Ixx)
+    # Ixyint = calculateIntegral(Ixy)
+    # Iyyint = calculateIntegral(Iyy)
+    # print(time.time() - start_time)
+    
+    # hessian = np.zeros((image.shape[0], image.shape[1], 2, 2))
+    # print("Calculating hessian...")
+    # start_time = time.time()
+    # for i in range(image.shape[0]):
+    #     for j in range(image.shape[1]):
+    #         xmax = i+int(patch/2)
+    #         ymax = j+int(patch/2)
+    #         xmin = i-int(patch/2)-1
+    #         ymin = j-int(patch/2)-1
+    #         if xmax>=image.shape[0]:
+    #             xmax = image.shape[0]-1
+    #         if ymax>=image.shape[1]:
+    #             ymax = image.shape[1]-1
             
-            sigmaIxx = Ixxint[xmax][ymax]
-            sigmaIyy = Iyyint[xmax][ymax]
-            sigmaIxy = Ixyint[xmax][ymax]
-            if xmin>=0 and ymin>=0:
-                sigmaIxx = sigmaIxx - Ixxint[xmax][ymin] - Ixxint[xmin][ymax] + Ixxint[xmin][ymin]
-                sigmaIyy = sigmaIyy - Iyyint[xmax][ymin] - Iyyint[xmin][ymax] + Iyyint[xmin][ymin]
-                sigmaIxy = sigmaIxy - Ixyint[xmax][ymin] - Ixyint[xmin][ymax] + Ixyint[xmin][ymin]
-            elif xmin>=0:
-                sigmaIxx = sigmaIxx - Ixxint[xmin][ymax]
-                sigmaIyy = sigmaIyy - Iyyint[xmin][ymax]
-                sigmaIxy = sigmaIxy - Ixyint[xmin][ymax]
-            elif ymin>=0:
-                sigmaIxx = sigmaIxx - Ixxint[xmax][ymin]
-                sigmaIyy = sigmaIyy - Iyyint[xmax][ymin]
-                sigmaIxy = sigmaIxy - Ixyint[xmax][ymin]
+    #         sigmaIxx = Ixxint[xmax][ymax]
+    #         sigmaIyy = Iyyint[xmax][ymax]
+    #         sigmaIxy = Ixyint[xmax][ymax]
+    #         if xmin>=0 and ymin>=0:
+    #             sigmaIxx = sigmaIxx - Ixxint[xmax][ymin] - Ixxint[xmin][ymax] + Ixxint[xmin][ymin]
+    #             sigmaIyy = sigmaIyy - Iyyint[xmax][ymin] - Iyyint[xmin][ymax] + Iyyint[xmin][ymin]
+    #             sigmaIxy = sigmaIxy - Ixyint[xmax][ymin] - Ixyint[xmin][ymax] + Ixyint[xmin][ymin]
+    #         elif xmin>=0:
+    #             sigmaIxx = sigmaIxx - Ixxint[xmin][ymax]
+    #             sigmaIyy = sigmaIyy - Iyyint[xmin][ymax]
+    #             sigmaIxy = sigmaIxy - Ixyint[xmin][ymax]
+    #         elif ymin>=0:
+    #             sigmaIxx = sigmaIxx - Ixxint[xmax][ymin]
+    #             sigmaIyy = sigmaIyy - Iyyint[xmax][ymin]
+    #             sigmaIxy = sigmaIxy - Ixyint[xmax][ymin]
 
-            hessian[i][j][0][0] = sigmaIxx
-            hessian[i][j][1][1] = sigmaIyy
-            hessian[i][j][1][0] = sigmaIxy
-            hessian[i][j][0][1] = sigmaIxy
+    #         hessian[i][j][0][0] = sigmaIxx
+    #         hessian[i][j][1][1] = sigmaIyy
+    #         hessian[i][j][1][0] = sigmaIxy
+    #         hessian[i][j][0][1] = sigmaIxy
+    # print(time.time() - start_time)
+    print("Calc hessian using scipy")
+    start_time = time.time()
+    Ixx = cnv(Ixx, np.ones((patch,patch)), mode='constant')
+    Iyy = cnv(Iyy, np.ones((patch,patch)), mode='constant')
+    Ixy = cnv(Ixy, np.ones((patch,patch)), mode='constant')
     print(time.time() - start_time)
-    return hessian
+    # newHessian = np.zeros((image.shape[0], image.shape[1], 2, 2))
+    # for i in range(image.shape[0]):
+    #     for j in range(image.shape[1]):
+    #         newHessian[i][j][0][0] = Ixx[i][j]
+    #         newHessian[i][j][1][1] = Iyy[i][j]
+    #         newHessian[i][j][1][0] = Ixy[i][j]
+    #         newHessian[i][j][0][1] = Ixy[i][j]
+    
+    # print(np.array_equal(hessian,newHessian))
+    return (Ixx*Iyy - Ixy**2)
+    # return Ixx,Ixy,Iyy
 
 def diagonalizeHessian(hess,resArray):
     print("Diagonalizing hessian...")
@@ -128,29 +145,29 @@ if __name__ == '__main__':
     img = cv2.imread('Chess_Board.png',cv2.IMREAD_GRAYSCALE)
     img = img.astype('int64')
     # start_time = time.time()
-    hessian = calculateHessian(img,patchSize)
-
+    hessianDet = calculateHessian(img,patchSize)
+    # hessianDet = 
 
 
     # hessianDet = diagonalizeHessian(hessian)
 
-    retArrays = []
-    for i in range(4):
-        retArrays.append(multiprocessing.Array('d', int(img.shape[0]*img.shape[1]/4)))
+    # retArrays = []
+    # for i in range(4):
+    #     retArrays.append(multiprocessing.Array('d', int(img.shape[0]*img.shape[1]/4)))
 
-    processes = []
-    for i in range(4):
-        processes.append(multiprocessing.Process(target = diagonalizeHessian, args=(hessian[int(i*hessian.shape[0]/4):int((i+1)*hessian.shape[0]/4)][:][:][:],retArrays[i])))
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
+    # processes = []
+    # for i in range(4):
+    #     processes.append(multiprocessing.Process(target = diagonalizeHessian, args=(hessian[int(i*hessian.shape[0]/4):int((i+1)*hessian.shape[0]/4)][:][:][:],retArrays[i])))
+    # for p in processes:
+    #     p.start()
+    # for p in processes:
+    #     p.join()
 
-    hessianDet = np.zeros(img.shape)
-    for i in range(4):
-        subsize  = int(img.shape[0]/4)
-        for j in range(int(img.shape[0]*img.shape[1]/4)):
-            hessianDet[(i*subsize)+(j//img.shape[1])] [j%img.shape[1]]= retArrays[i][j]
+    # hessianDet = np.zeros(img.shape)
+    # for i in range(4):
+    #     subsize  = int(img.shape[0]/4)
+    #     for j in range(int(img.shape[0]*img.shape[1]/4)):
+    #         hessianDet[(i*subsize)+(j//img.shape[1])] [j%img.shape[1]]= retArrays[i][j]
 
     # print(time.time() - start_time)
 
